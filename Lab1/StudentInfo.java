aa4787bf65ef16b569b273a8dda6c9087289f547
Constantinescu George-Gabriel <57069862+iceeye7gabi@users.noreply.github.com>
2021-02-15 11:22:12 +0200

    Update StudentInfo.java

--- Lab1/StudentInfo.java
+++ Lab1/StudentInfo.java
@@ -1,5 +1,12 @@
-class StudentInfo extends Student{             
+class StudentInfo extends Student{
+    String nameStudent;             
     public void invatMaterie(){
         System.out.println("Invat programare! :)");
+    }
+    public void getnameStudent(){
+        System.out.println("Studentul care invata Informatica este:"+nameStudent);
+    }
+    public void setnameStudent(String name){
+        nameStudent=name;
     }
 }
