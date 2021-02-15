4ecf164e92e0026a6621720904ae8ad3fb7b6451
Constantinescu George-Gabriel <57069862+iceeye7gabi@users.noreply.github.com>
2021-02-15 11:22:25 +0200

    Update StudentMate.java

--- Lab1/StudentMate.java
+++ Lab1/StudentMate.java
@@ -1,5 +1,12 @@
 class StudentMate extends Student{
+    String nameStudent; 
     public void invatMaterie(){
         System.out.println("Invat mate! :)");
+    }
+    public void getnameStudent(){
+        System.out.println("Studentul care invata Matematica este:"+nameStudent);
+    }
+    public void setnameStudent(String name){
+        nameStudent=name;
     }
 }
