39867a996324bbd257197c829903262276565874
Constantinescu George-Gabriel <57069862+iceeye7gabi@users.noreply.github.com>
2021-02-15 11:23:14 +0200

    Update Main.java

--- Lab1/Main.java
+++ Lab1/Main.java
@@ -3,17 +3,28 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        StudentInfo Daniel = new StudentInfo();
-        Daniel.relaxStudent();
-        Daniel.invatMaterie();
-        
-        StudentMate Mihai = new StudentMate();
-        Mihai.invatMaterie();
+         StudentInfo stud1 = new StudentInfo();
+        stud1.setnameStudent("Daniel Bogatu");
+        stud1.getnameStudent();
+        stud1.relaxStudent();
+        stud1.invatMaterie();
+
+        StudentMate stud2 = new StudentMate();
+        stud2.setnameStudent("Gabriel Constantinescu");
+        stud2.getnameStudent();
+        stud2.relaxStudent();
+        stud2.invatMaterie();
+
+
 
         ProfesorInfo prof1 = new ProfesorInfo("Olariu");
         prof1.getnameProfesor();
+        prof1.setnameProfesor("Gabriel");
+        prof1.getnameProfesor();
 
         ProfesorMate prof2 = new ProfesorMate("Zalinescu");
+        prof2.getnameProfesor();
+        prof2.setnameProfesor("Daniel");
         prof2.getnameProfesor();
 
         Math calcule=new Math();
